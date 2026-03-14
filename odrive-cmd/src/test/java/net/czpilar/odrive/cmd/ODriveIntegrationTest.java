9945d23b3af44202bd64416010d5489f552bc304
czpilar <david@czpilar.net>
2026-03-14 12:45:42 +0100

    added integration test for complete scenario testing

---
+++ odrive-cmd/src/test/java/net/czpilar/odrive/cmd/ODriveIntegrationTest.java
@@ -0,0 +1,71 @@
+package net.czpilar.odrive.cmd;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+/**
+ * @author David Pilar (david@czpilar.net)
+ */
+public class ODriveIntegrationTest {
+
+    private static final String PROPERTIES = "odrive.properties";
+
+    @Test
+    public void testNoArgs() {
+        ODrive.main(new String[]{});
+    }
+
+    @Test
+    public void testHelp() {
+        ODrive.main(new String[]{"-h"});
+    }
+
+    @Test
+    public void testVersion() {
+        ODrive.main(new String[]{"-v"});
+    }
+
+    @Test
+    public void testProperties() {
+        ODrive.main(new String[]{"-p", PROPERTIES});
+    }
+
+    @Test
+    public void testShowAuthLink() {
+        ODrive.main(new String[]{"-l", "-p", PROPERTIES});
+    }
+
+    @Test
+    public void testAuthorize() {
+        ODrive.main(new String[]{"-a", "auth_code", "-p", PROPERTIES});
+    }
+
+    private void createFileIfNotExist(String filename) throws IOException {
+        File file = new File(filename);
+        if (!file.exists()) {
+            FileUtils.writeStringToFile(file, "This is a testing file with filename: " + filename, Charset.defaultCharset());
+        }
+    }
+
+    @Test
+    public void testUploadFiles() throws IOException {
+        String filename1 = "target/test1.txt";
+        String filename2 = "target/test2.txt";
+        String filename3 = "target/test3.txt";
+        createFileIfNotExist(filename1);
+        createFileIfNotExist(filename2);
+        createFileIfNotExist(filename3);
+        ODrive.main(new String[]{"-f", filename1, filename2, filename3, "-d", "odrive-test-backup", "-p", PROPERTIES});
+    }
+
+    @Test
+    public void testUploadFileToSubdirectory() throws IOException {
+        String filename = "target/test1.txt";
+        createFileIfNotExist(filename);
+        ODrive.main(new String[]{"-f", filename, "-d", "odrive-test-backup/odrive-subdir/odrive-last-dir", "-p", PROPERTIES});
+    }
+}
