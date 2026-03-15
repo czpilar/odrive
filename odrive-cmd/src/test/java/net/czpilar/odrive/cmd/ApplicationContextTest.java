1031c6c36867497cf58dc091c1cf191db5f0214a
czpilar <david@czpilar.net>
2026-03-15 14:40:38 +0100

    added test

---
+++ odrive-cmd/src/test/java/net/czpilar/odrive/cmd/ApplicationContextTest.java
@@ -0,0 +1,19 @@
+package net.czpilar.odrive.cmd;
+
+import net.czpilar.odrive.cmd.context.ODriveCmdContext;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * @author David Pilar (david@czpilar.net)
+ */
+@ExtendWith(SpringExtension.class)
+@Import(ODriveCmdContext.class)
+public class ApplicationContextTest {
+
+    @Test
+    void test() {
+    }
+}
